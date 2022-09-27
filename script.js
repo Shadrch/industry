7fad2a3a08e6587fbd6bb08c0102248c225faccc
Shadrch <114035756+Shadrch@users.noreply.github.com>
2022-09-27 10:07:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+ function sub(){
+     document.querySelector(".submit").style.display = "block";
+    document.querySelector(".cart").style.display = "none";
+ }
+ function submited(){
+     document.querySelector(".submit").style.display = "none";
+    document.querySelector(".recieved").style.display = "block";
+ }
+
+
+
+
+ function suba(){
+    document.querySelector(".submita").style.display = "block";
+   document.querySelector(".carta").style.display = "none";
+}
+function submiteda(){
+    document.querySelector(".submiat").style.display = "none";
+   document.querySelector(".recieveda").style.display = "block";
+}
+
+
+function subb(){
+    document.querySelector(".submitb").style.display = "block";
+   document.querySelector(".cartb").style.display = "none";
+}
+function submitedb(){
+    document.querySelector(".submitb").style.display = "none";
+   document.querySelector(".recievedb").style.display = "block";
+}
+
+
+
+function subc(){
+    document.querySelector(".submitc").style.display = "block";
+   document.querySelector(".cartc").style.display = "none";
+}
+function submitedc(){
+    document.querySelector(".submitc").style.display = "none";
+   document.querySelector(".recievedc").style.display = "block";
+}
